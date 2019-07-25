052464cc3dd840606310478a1f84698e26376a6d
thainv-0029 <nguyen.viet.thai@sun-asterisk.com>
2019-07-25 08:47:40 +0700

    Create Migrate

---
+++ database/migrations/2019_07_24_025802_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('receiver');
+            $table->string('receiver_address');
+            $table->string('payment');
+            $table->text('message')->nullable();
+            $table->double('total', 5, 2);
+            $table->bigInteger('order_status')->unsigned();
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
